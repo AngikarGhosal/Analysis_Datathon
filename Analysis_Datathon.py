0c2a3b55a1a9baf35b4f6d76fdd58b9a54c12883
AngikarGhosal <52449176+AngikarGhosal@users.noreply.github.com>
2019-11-02 12:39:42 -0400

    Updated

--- Analysis_Datathon.py
+++ Analysis_Datathon.py
@@ -5,6 +5,7 @@ Created on Sat Nov  2 10:14:07 2019
 @author: angik
 """
 import pandas
+import ast
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -26,4 +27,46 @@ valfile = "validation.csv"
 infodata = pandas.read_csv(infofile)
 traindata = pandas.read_csv(trainfile)
 valdata = pandas.read_csv(valfile)
-print(infodata.head(20))
+'''
+traindata['ltiFeatures']=traindata['ltiFeatures'].apply(ast.literal_eval)
+traindata['stiFeatures']=traindata['stiFeatures'].apply(ast.literal_eval)
+valdata['ltiFeatures']=valdata['ltiFeatures'].apply(ast.literal_eval)
+valdata['stiFeatures']=valdata['stiFeatures'].apply(ast.literal_eval)
+'''
+''' Till this much needs to remain as it is, feel free to change remaining'''
+
+
+#print(infodata.head(20))
+
+
+#The following code breaks down interests as per depth level.
+'''
+x={1:[],2:[],3:[],4:[],5:[],6:[]}
+for index, row in infodata.iterrows():
+    string=row['topic_name']
+    depth=string.count('/')
+    x[depth].append(string)
+
+print (x[1])
+print ('These are the broad topics')
+'''
+
+
+#The following code shows that you are more likely to not have short term interests, if you are a convert.
+'''
+
+nostifalse=0
+nostitrue=0
+for index, row in traindata.iterrows():
+    dic=row['stiFeatures']
+    k=row['userID']
+    if (len(dic)==2):
+        if (k<1500):
+            nostitrue=nostitrue+1
+        else:
+            nostifalse=nostifalse+1
+            
+print (nostitrue, " out of 1465 ", nostitrue/1465)
+print (nostifalse, " out of 94941", nostifalse/94941)
+
+'''
