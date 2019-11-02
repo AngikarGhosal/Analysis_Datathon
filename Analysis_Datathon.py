1b784136a52e1079cd3340a7662e223c5e710e77
AngikarGhosal <52449176+AngikarGhosal@users.noreply.github.com>
2019-11-02 14:02:14 -0400

    hello

--- Analysis_Datathon.py
+++ Analysis_Datathon.py
@@ -27,6 +27,15 @@ valfile = "validation.csv"
 infodata = pandas.read_csv(infofile)
 traindata = pandas.read_csv(trainfile)
 valdata = pandas.read_csv(valfile)
+
+LR=LogisticRegression(solver='liblinear',multi_class='ovr')
+LDA=LinearDiscriminantAnalysis()
+KNN=KNeighborsClassifier()
+CART=DecisionTreeClassifier()
+NB=GaussianNB()
+SVM=SVC(gamma='auto')
+
+
 '''
 traindata['ltiFeatures']=traindata['ltiFeatures'].apply(ast.literal_eval)
 traindata['stiFeatures']=traindata['stiFeatures'].apply(ast.literal_eval)
@@ -43,14 +52,18 @@ valdata['stiFeatures']=valdata['stiFeatures'].apply(ast.literal_eval)
 
 x={1:[],2:[],3:[],4:[],5:[],6:[]}
 y={}
+w={1:[],2:[],3:[],4:[],5:[],6:[]}
+v={}
+u={}
 for index, row in infodata.iterrows():
     string=row['topic_name']
     id=row['topic_id']
     depth=string.count('/')
     x[depth].append(id)
+    w[depth].append(string)
     y[id]=depth
-
-
+    v[string]=depth
+    u[string]=id
 
 
 #The following code shows that you are more likely to not have short term interests, if you are a convert.
@@ -72,7 +85,7 @@ print (nostifalse, " out of 94941", nostifalse/94941)
 
 '''
 
-#The follwoing code shows that the avg depth of long term interests does not influence conversion
+#The following code shows that the avg depth of long term interests does not influence conversion
 '''
 meanweighttrue=0
 meanweightfalse=0
@@ -95,4 +108,49 @@ meanweightfalse=meanweightfalse/94941
 print (meanweighttrue)
 print (meanweightfalse)
 '''
+
+
+
+#Same as previous, but for short term interests
+'''
+meanweighttrue=0
+meanweightfalse=0
+for index, row in traindata.iterrows():
+    lti=row['stiFeatures']
+    lti=ast.literal_eval(lti)
+    ltikeys=lti.keys()
+    weightltikeys=0
+    for i in ltikeys:
+        j=int(i)
+        if (j in y.keys()):
+            weightltikeys=weightltikeys+ (y[j]/6)*lti[i]
+    k=row['userID']
+    if (k<1500):
+        meanweighttrue=meanweighttrue+weightltikeys
+    else:
+        meanweightfalse=meanweightfalse+weightltikeys
+meanweighttrue=meanweighttrue/1465
+meanweightfalse=meanweightfalse/94941
+print (meanweighttrue)
+print (meanweightfalse)
+'''
+z={}
+for i in w[1]:
+    z[i]=i
+for index, row in infodata.iterrows():
+    string=row['topic_name']
+    if (string not in z.keys()):
+        sh=string[1:]
+        ind=sh.find('/')
+        st=string[0:ind+1]
+        z[string]=st
+
+    
+qtr={}
+for i in z.keys():
+    qtr[u[i]]=u[z[i]]
+
+print (qtr)
+
+
 
